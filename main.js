8c9c12a71025645476d7be5e226a45e985428493
divaaga <93255055+divaaga@users.noreply.github.com>
2023-10-13 22:05:14 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+var leftWristX, rightWristX, difference;
+
 function setup() {
     canvas = createCanvas(400, 400);
     canvas.position(510, 100);
@@ -13,12 +15,23 @@ function setup() {
 function gotPoses(results) {
     if(results.length > 0) {
         console.log(results);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        leftWristX = results[0].pose.leftWrist.x;
+        difference = floor(leftWristX - rightWristX);
+        console.log("rightWristX = " + rightWristX + " leftWristX = " + leftWristX + " Diffrence between the two wrists = " + difference);
     }
 }
 
 function modelLoaded() {
+    console.log("poseNet has initialized...")
 }
 
 function draw(){
-    background("#6cb8eb"); 
+    background("#6cb8eb");
+    
+    document.getElementById("size").innerHTML = "Width and height of the square = " + difference + "px";
+    textSize(difference);
+    fill("yellow");
+    text("DIVA", 200, 200);
 }
